52570d9ddb60badb94347015cfcda887741049ff
Tnasrul <nasrultoriq@gmail.com>
2023-09-19 22:27:28 +0700

    add category fragment

--- app/src/main/java/com/dicoding/myflexiblefragment/CategoryFragment.kt
+++ app/src/main/java/com/dicoding/myflexiblefragment/CategoryFragment.kt
@@ -6,27 +6,23 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.dicoding.myflexiblefragment.R
 
-class HomeFragment : Fragment(), View.OnClickListener {
+class CategoryFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_category, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnCategory: Button = view.findViewById(R.id.btn_category)
-        btnCategory.setOnClickListener(this)
+        val btnDetailCategory: Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
     }
-    override fun onClick(v: View) {
-        val categoryFragment = CategoryFragment()
-        val fragmentManager = parentFragmentManager
-        fragmentManager.beginTransaction().apply {
-            replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
-            addToBackStack(null)
-            commit()
-        }
 
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_detail_category) {
+
+        }
     }
 }
